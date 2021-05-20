ecf0a0c9f558eac87207072f61c2222de70a4e69
LeonardoSoaresNunes <leonardoeaf2009@hotmail.com>
2021-05-20 16:53:41 -0300

    Implementando o JS

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let reultado = document.getElementById("resultado");
+
+function calcular(){
+    console.log("calculando..");
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+
+
+
+    let qtdTotalCarne = 400 *  adultos +(400/ 2 * criancas) ;
+    console.log(qtdTotalCarne);
+
+
+}
+
+
+function carnePP (duracao){
+    if (duracao >= 6){
+        return 650;
+
+    }
+  } else {
+    return 400;
+}
+
+
+
+console.log(inputAdultos.value , inputCriancas.value , inputDuracao.value);
