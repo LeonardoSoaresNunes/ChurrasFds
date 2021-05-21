7b50d50dd6a0f8fe6ac2f907be2c69a3cd7181e9
LeonardoSoaresNunes <leonardoeaf2009@hotmail.com>
2021-05-21 16:46:47 -0300

    Testes Finais e Corrreções Simples do Projeto

--- js/script.js
+++ js/script.js
@@ -11,17 +11,36 @@ function calcular(){
     let criancas = inputCriancas.value;
     let duracao = inputDuracao.value;
 
-    let qtdTotalCarne = carnePP(duracao) *  adultos +(400/ 2 * criancas) ;
-    console.log(qtdTotalCarne);
+    let qtdTotalCarne = carnePP(duracao) *  adultos +(carnePP(duracao) / 2 * criancas) ;
+    let qtdTotalCerveja = cervejaPP(duracao) * adultos;
+    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+
+    resultado.innerHtml = '<p>${qtdTotalCarne}g de Carne</p>'
+    resultado.innerHtml +='<p>${qtdTotalCerveja}ml de Cerveja</p>'
+    resultado.innerHtml +='<p>${qtdTotalBebidas}ml de Bebidas</p>'
 }
 
 function carnePP (duracao){
    if (duracao >= 6){
        return 650;
-
     }else {
-    return 400;
+        return 400;
     }
 }
+function cervejaPP (duracao){
+    if (duracao >= 6){
+        return 2000;
+      }else {
+         return 1200;
+     }
+ }
 
-console.log(inputAdultos.value , inputCriancas.value , inputDuracao.value);
+ function bebidasPP (duracao){
+    if (duracao >= 6){
+        return 1500;
+      }else {
+         return 1000;
+     }
+ }
+
+//console.log(inputAdultos.value , inputCriancas.value , inputDuracao.value);
